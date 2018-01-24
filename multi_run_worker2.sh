2d99d692951da2b9c76a13186410b52ccb5a29f0
Yu-Chih Tung <yctung@umich.edu>
2018-01-24 16:17:36 -0500

    update multi machine run scripts

---
+++ multi_run_worker2.sh
@@ -0,0 +1,6 @@
+source ./multi_setting.sh
+
+python ptb/ptb_word_lm_dist.py --data_path=example/data/ --model=huge --device=cpu --num_gpus=0 \
+     --ps_hosts=$HOST_PS \
+     --worker_hosts=$HOST_WORKER_1,$HOST_WORKER_2 \
+     --job_name=worker --task_index=1
